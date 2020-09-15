ca619e656178d366d3ee61737bdd854e96089045
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-15 21:10:20 +0800

    [pyq] feat : 1-requirement_completed

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,32 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
     @Test
-    void contextLoads() {
+    void should_get_a_hs() throws Exception {
+        mockMvc.perform(get("/hs/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("第一条事件"));
+        mockMvc.perform(get("/hs/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("第二条事件"));
+        mockMvc.perform(get("/hs/3"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("第三条事件"));
     }
+
 
 }
