29e7bbd04087204256745cbddfa8c192bbd7af1f
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-15 23:59:46 +0800

    [pyq] feat : 3-requirement_completed

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.dto.hs;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,39 +9,95 @@ import java.util.List;
 
 @RestController
 public class RsController {
-    private List<String> rsList = initList();
 
-//    private List<String> hsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
-//    private List<String> tempList = new ArrayList<>();
-    private List<String> initList(){
-        List<String> tempList = new ArrayList<>();
-        tempList.add("第一条事件");
-        tempList.add("第二条事件");
-        tempList.add("第三条事件");
-        return tempList;
+
+    private List<hs> hsList = init2List();
+
+    private List<hs> init2List() {
+        List<hs> temp2List = new ArrayList<>();
+        temp2List.add(new hs("第一条事件","无分类"));
+        temp2List.add(new hs("第二条事件","无分类"));
+        temp2List.add(new hs("第三条事件","无分类"));
+        return temp2List;
     }
+//======================================================
+//        private List<String> tempList = new ArrayList<>();
+//    private List<String> rsList = initList();
+//
+//    private List<String> initList() {
+//        List<String> tempList = new ArrayList<>();
+//        tempList.add("第一条事件");
+//        tempList.add("第二条事件");
+//        tempList.add("第三条事件");
+//        return tempList;
+//    }
+//
+//    @GetMapping("/hs/{index}")
+//    public String get_a_hs(@PathVariable String index) {
+//        return rsList.get(Integer.parseInt(index) - 1);
+//    }
+//
+//    @GetMapping("/hs/rg")
+//    public String get_a_list_of_hs(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+//        if (start == null || end == null) {
+//            return rsList.toString();
+//        }
+//        return rsList.subList(start - 1, end).toString();
+//    }
+//
+//    @GetMapping("/hs/list")
+//    public String get_all_hs() {
+//        return rsList.toString();
+//    }
+//
+//    @PostMapping("/hs/event")
+//    public void add_a_hs(@RequestBody String temp) {
+//        rsList.add(temp);
+//    }
+//    =======================================================================
+
+
+
+//    private List<hs> initList() {
+//        List<String> tempList = new ArrayList<>();
+//        tempList.add("第一条事件");
+//        tempList.add("第二条事件");
+//        tempList.add("第三条事件");
+//        return tempList;
+//    }
+
     @GetMapping("/hs/{index}")
-    public String get_a_hs(@PathVariable String index) {
-        return rsList.get(Integer.parseInt(index) - 1);
+    public hs get_a_hs(@PathVariable String index) {
+        return hsList.get(Integer.parseInt(index) - 1);
     }
 
     @GetMapping("/hs/rg")
-    public String get_a_list_of_hs(@RequestParam(required = false) Integer start,@RequestParam(required = false) Integer end) {
-      if (start==null || end==null){
-        return rsList.toString();
-      }
-        return rsList.subList(start-1, end).toString();
+    public String get_a_list_of_hs(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+        if (start == null || end == null) {
+            return hsList.toString();
+        }
+        return hsList.subList(start - 1, end).toString();
     }
 
     @GetMapping("/hs/list")
-    public String get_all_hs() {
-        return rsList.toString();
+    public List<hs> get_all_hs() {
+        return hsList;
     }
 
     @PostMapping("/hs/event")
-    public void add_a_hs(@RequestBody String temp) {
-         rsList.add(temp);
-
+    public void add_a_hs( @RequestBody hs temp) {
+        hsList.add(temp);
     }
+
+
+
+
+
+
+
+
+
+
+
 }
 
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,5 +56,26 @@ class RsListApplicationTests {
                 .andExpect(jsonPath("$", hasSize(4)));
     }
 
+    @Test
+    void should_add_hs() throws Exception {
+        mockMvc.perform(get("/hs/list"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+
+        mockMvc.perform(post("/hs/event").content("{\"hs_name\":\"猪肉涨价了\",\"key\":\"经济\"}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+         mockMvc.perform(get("/hs/list"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].hs_name",is("第一条事件")))
+                .andExpect(jsonPath("$[0].key",is("无分类")))
+                .andExpect(jsonPath("$[1].hs_name",is("第二条事件")))
+                .andExpect(jsonPath("$[1].key",is("无分类")))
+                .andExpect(jsonPath("$[2].hs_name",is("第三条事件")))
+                .andExpect(jsonPath("$[2].key",is("无分类")))
+                .andExpect(jsonPath("$[3].hs_name",is("猪肉涨价了")))
+                .andExpect(jsonPath("$[3].key",is("经济")));
+    }
 
 }
