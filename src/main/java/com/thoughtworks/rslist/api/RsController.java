a45b2f69489797b5f74cc266737133ad9ce8aafa
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-10-07 17:16:25 +0800

    [pyq]feat:implement the data-binding branch

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,44 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.dto.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@RequestMapping(path="/rs", produces = "application/json; charset=utf-8")
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+//  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private final List<RsEvent> rsList;
+
+  public RsController(){
+    rsList=new ArrayList<RsEvent>(){{
+      add(new RsEvent("第一条事件","无分类"));
+      add(new RsEvent("第二条事件","无分类"));
+      add(new RsEvent("第三条事件","无分类"));
+    }};
+  }
+
+  @PutMapping("/event/{index}")
+  public RsEvent updateRsEvent(@RequestBody RsEvent rsEvent, @PathVariable int index){
+    index--;
+    validateIndex(index);
+    rsList.set(index,rsEvent);
+    return rsEvent;
+  }
+
+  @DeleteMapping("/event/{index}")
+  public RsEvent deleteRsEvent(@PathVariable int index){
+    index--;
+    validateIndex(index);
+    return rsList.remove(index);
+  }
+
+  private void validateIndex(int index){
+    if (index<0||index>=rsList.size()){
+      throw new RuntimeException("index out of range");
+    }
+  }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,56 @@
 package com.thoughtworks.rslist;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.dto.RsEvent;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode=DirtiesContext.ClassMode
+.BEFORE_EACH_TEST_METHOD)
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void should_update_rs_event() throws Exception {
+        String updateRsEventJson=stringify(new RsEvent("第一条事件","分类1"));
+
+        mockMvc.perform(put("/rs/event/1")
+        .content(updateRsEventJson)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().string(updateRsEventJson));
     }
 
+    @Test
+    void should_delete_rs_event() throws Exception {
+        String deleteRsEventJson=stringify(new RsEvent("第一条事件","无分类"));
+
+        mockMvc.perform(delete("/rs/event/1")).andExpect(status()
+        .isOk()).andExpect(content().string(deleteRsEventJson));
+    }
+
+    private String stringify(RsEvent rsevent) throws JsonProcessingException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        return objectMapper.writeValueAsString(rsevent);
+    }
+
+//    private RsEvent parse(String rsEventJson) throws JsonProcessingException {
+//        ObjectMapper objectMapper=new ObjectMapper();
+//        return objectMapper.readValue(rsEventJson,RsEvent.class);
+//    }
 }
